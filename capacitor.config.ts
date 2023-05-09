22e7acf47eae5ddd9577cb84f45f09e562465702
Jon <jon.herman@ionic.io>
2023-05-09 08:53:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jon-angular-cap-5',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
